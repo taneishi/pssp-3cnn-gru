126128f44927027fb53dcbb2610ed32669f047bd
taneishi <taneishi@users.noreply.github.com>
2020-02-06 16:58:58 +0900

    show PBS hostname

--- pssp-nn/run.sh
+++ pssp-nn/run.sh
@@ -2,9 +2,9 @@
 #PBS -o log/$PBS_JOBID.out
 #PBS -j oe
 
-hostname | grep '^s'
 lscpu | grep 'Model name\\|^CPU(s)'
 
+if [ $PBS_O_HOST ]; then echo $PBS_O_HOST; fi
 if [ $PBS_O_WORKDIR ]; then cd $PBS_O_WORKDIR; mkdir -p log; fi
 
 if [ -d /opt/intel/inteloneapi ]; then 
