98ee180265c7794cf2737b12d6e405b08d02a767
taneishi <taneishi@users.noreply.github.com>
2022-03-07 16:40:03 +0900

    appended requirements.txt.

--- run.sh
+++ run.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 mkdir -p data
-wget -c -P data http://www.princeton.edu/~jzthree/datasets/ICML2014/cullpdb+profile_6133_filtered.npy.gz
 wget -c -P data http://www.princeton.edu/~jzthree/datasets/ICML2014/cullpdb+profile_5926_filtered.npy.gz
 wget -c -P data http://www.princeton.edu/~jzthree/datasets/ICML2014/cb513+profile_split1.npy.gz
 
-if [ -d torch ]; then
-    source torch/bin/activate
-else
-    python3 -m venv torch
-    source torch/bin/activate
-    pip install --upgrade pip
-    pip install torch numpy pandas matplotlib
-fi
+pip install -r requirements.txt
 
 python stats.py
 python main.py
